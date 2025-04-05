6fb764db4e5840c067d4ff820e231eb6a08464e4
Shi-D <1152460717@qq.com>
2025-04-05 13:29:01 -0500

    update

--- MUSE/load_data.py
+++ MUSE/load_data.py
@@ -2,39 +2,39 @@ import os
 from datasets import load_dataset
 from utils import write_json, write_text
 
-os.makedirs('/lus/eagle/projects/PBML/yingdan/data', exist_ok=True)
+os.makedirs('data', exist_ok=True)
 
 for corpus, Corpus in zip(['news', 'books'], ['News', 'Books']):
     for split in ['forget_qa', 'retain_qa', 'forget_qa_icl', 'retain_qa_icl']:
-        data = load_dataset(f"/lus/eagle/projects/PBML/yingdan/muse-bench/MUSE-{Corpus}", 'knowmem', split=split)
+        data = load_dataset(f"muse-bench/MUSE-{Corpus}", 'knowmem', split=split)
         questions, answers = data['question'], data['answer']
         knowmem = [
             {'question': question, 'answer': answer}
             for question, answer in zip(questions, answers)
         ]
-        write_json(knowmem, f"/lus/eagle/projects/PBML/yingdan/data/{corpus}/knowmem/{split}.json")
+        write_json(knowmem, f"data/{corpus}/knowmem/{split}.json")
 
     for split in ['forget']:
-        data = load_dataset(f"/lus/eagle/projects/PBML/yingdan/muse-bench/MUSE-{Corpus}", 'verbmem', split='forget')
+        data = load_dataset(f"muse-bench/MUSE-{Corpus}", 'verbmem', split='forget')
         prompts, gts = data['prompt'], data['gt']
         verbmem = [
             {'prompt': prompt, 'gt': gt}
             for prompt, gt in zip(prompts, gts)
         ]
-        write_json(verbmem, f"/lus/eagle/projects/PBML/yingdan/data/{corpus}/verbmem/forget.json")
+        write_json(verbmem, f"data/{corpus}/verbmem/forget.json")
 
     for split in ['forget', 'retain', 'holdout']:
-        privleak = load_dataset(f"/lus/eagle/projects/PBML/yingdan/muse-bench/MUSE-{Corpus}", 'privleak', split=split)['text']
-        write_json(privleak, f"/lus/eagle/projects/PBML/yingdan/data/{corpus}/privleak/{split}.json")
+        privleak = load_dataset(f"muse-bench/MUSE-{Corpus}", 'privleak', split=split)['text']
+        write_json(privleak, f"data/{corpus}/privleak/{split}.json")
 
     for split in ['forget', 'holdout', 'retain1', 'retain2']:
-        raw = load_dataset(f"/lus/eagle/projects/PBML/yingdan/muse-bench/MUSE-{Corpus}", 'raw', split=split)['text']
-        write_json(raw, f"/lus/eagle/projects/PBML/yingdan/data/{corpus}/raw/{split}.json")
-        write_text("\n\n".join(raw), f"/lus/eagle/projects/PBML/yingdan/data/{corpus}/raw/{split}.txt")
+        raw = load_dataset(f"muse-bench/MUSE-{Corpus}", 'raw', split=split)['text']
+        write_json(raw, f"data/{corpus}/raw/{split}.json")
+        write_text("\n\n".join(raw), f"data/{corpus}/raw/{split}.txt")
 
 
 for crit in ['scal', 'sust']:
     for fold in range(1, 5):
-        data = load_dataset(f"/lus/eagle/projects/PBML/yingdan/muse-bench/MUSE-News", crit, split=f"forget_{fold}")['text']
-        write_json(data, f"/lus/eagle/projects/PBML/yingdan/data/news/{crit}/forget_{fold}.json")
-        write_text("\n\n".join(data), f"/lus/eagle/projects/PBML/yingdan/data/news/{crit}/forget_{fold}.txt")
+        data = load_dataset(f"muse-bench/MUSE-News", crit, split=f"forget_{fold}")['text']
+        write_json(data, f"data/news/{crit}/forget_{fold}.json")
+        write_text("\n\n".join(data), f"data/news/{crit}/forget_{fold}.txt")
