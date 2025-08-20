d5cdf3483dcc51da7e09dc26fd2aef4b242226fb
Shi-D <1152460717@qq.com>
2025-08-20 16:23:01 -0500

    update

--- MUSE/baselines/baselines/iterative.py
+++ MUSE/baselines/baselines/iterative.py
@@ -63,8 +63,8 @@ def unlearn(
         num_train_epochs=epochs,
         optim='adamw_torch',
         lr_scheduler_type='constant',
-        bf16=False,
-        fp16=True,
+        # bf16=False,
+        # fp16=True,
         report_to='none',        # Disable wandb
         # gradient_accumulation_steps=4,
         gradient_checkpointing=True,
